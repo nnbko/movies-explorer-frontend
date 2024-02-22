409076e980136860573ea3283e382c31380bd909
nnbko <dadaya999@yandex.ru>
2024-02-23 01:46:35 +0300

    fix constants, fix cards

--- movies-front/src/utils/Constants.js
+++ movies-front/src/utils/Constants.js
@@ -1,10 +1,10 @@
 export const Short_Films = 40
 export const Screen_size_S = 320
-export const Screen_size_M = 768
-export const Screen_size_B = 1280
-export const Cards_count_B = 16;
+export const Screen_size_M = 631
+export const Screen_size_B = 1100
+export const Cards_count_B = 12;
 export const Cards_count_M = 8;
 export const Cards_count_S = 5;
-export const Add_cards_B = 4;
+export const Add_cards_B = 3;
 export const Add_cards_M = 2;
 export const Add_cards_S = 2;
